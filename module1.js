f5a02c457774c273ab0ac2c7833993ef12585bcc
Navyaa Mathur <navyaamathur@yahoo.com>
2016-07-14 14:20:36 +0100

    Test online

--- module1.js
+++ module1.js
@@ -28,12 +28,102 @@ function getRandomInt(min, max) {
 
 // function definitions here
 
+//pixels[((i*pixels.width + j)*4)+n]
+//i is the row, j is the column, n is the rgba so 0-3
+
+function makeBlue(pixels, adjust){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    data[i+2]+=adjust;
+  }
+  return pixels;
+}
+
+function makeRed(pixels, adjust){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    data[i]+=adjust;
+  }
+  return pixels;
+}
+
+function makeGreen(pixels, adjust){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    data[i+1]+=adjust;
+  }
+  return pixels;
+}
+
+function makeBright(pixels, adjust){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    data[i]+=adjust;
+    data[i+1]+=adjust;
+    data[i+2]+=adjust;
+  }
+  return pixels;
+}
+
+function invert(pixels){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    data[i]=255-data[i];
+    data[i+1]=255-data[i+1];
+    data[i+2]=255-data[i+2];
+  }
+  return pixels;
+}
+
+function noisemaker(pixels){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for(var i = 0; i < length; i+=4){
+    var adjust=getRandomInt(-80, 80);
+    data[i]+=adjust;
+    data[i+1]+=adjust;
+    data[i+2]+=adjust;
+  }
+  return pixels;
+}
+
+function makeVaguelyTrippy(pixels){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  for (var i=0; i<length/2; i+=2){
+    var temp = data[i];
+    data[i] = data[length-i];
+    data[length-i] = temp;
+  }
+  return pixels;
+}
+
+function rotate(pixels){
+  var length = pixels.data.length;
+  var data = pixels.data;
+  //move pixel by pixel not value by value
+  for (var i=0; i<length/2; i+=4){
+    var temp = [data[i], data[i+1], data[i+2], data[i+3]];
+    //swap entire pixel
+    for(var j=0; j<4; j++){
+      data[i+j]=data[(length-i)+j];
+      data[(length-i)+j]=temp[j];
+    }
+  }
+  return pixels;
+}
 
 $(document).ready(function() {
     var img = new Image();
     img.src = "img/cat.jpg";
-
-
-
-
-});
+    var rawPixels = ImageUtils.getPixels(img);
+    var noisePixels = noisemaker(rawPixels);
+    var pixels = rotate(noisePixels);
+    ImageUtils.putPixels(pixels, pixels.width, pixels.height);
+  }
+);
--- module2.js
+++ module2.js
@@ -19,20 +19,52 @@ class ImageUtils {
         var ctx = c.getContext('2d');
         ctx.putImageData(imageData, 0, 0);
     }
-
 }
+
+class RGBA{
+    constructor(redValue, greenValue, blueValue, alphaValue){
+      this.red = redValue;
+      this.green = greenValue;
+      this.blue = blueValue;
+      this.alpha = alphaValue;
+    }
+  }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function definitions here
+function colourise(img, colour, level){
+  var pixels = ImageUtils.getPixels(img);
+  var all = pixels.data.length;
+  var data = pixels.data;
+  for (var i=0; i<all; i+=4){
+    var original = new RGBA(data[i], data[i+1], data[i+2], data[i+3]);
+    var modified=colourisePixel(original, colour, level);
+    data[i]=modified.red;
+    data[i+1]=modified.green;
+    data[i+2]=modified.blue;
+    data[i+3]=modified.alpha;
+  }
+
+  console.log(pixels);
+  ImageUtils.putPixels(pixels, img.width, img.length);
+}
+
+function colourisePixel(original, colour, level){
+  var modifiedRed = (1-level) * original.red + level * colour.red;
+  var modifiedBlue = (1-level) * original.blue + level * colour.blue;
+  var modifiedGreen = (1-level) * original.green + level * colour.green;
+  var modifiedAlpha = original.alpha - ((original.alpha - colour.alpha) * level);
+  return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, modifiedAlpha);
+}
 
 $(document).ready(function() {
     var img = new Image();
     img.src = "img/cat.jpg";
-
-
-
-
+    //var pixels = ImageUtil.getPixels(img);
+    var colour = new RGBA(213, 23, 88, 255);
+    //console.log(colour);
+    colourise(img, colour, 1);
 });
